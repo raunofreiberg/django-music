1056fb0de4e34a2af65d65df8d75231f284d703f
raunofreiberg <rauno@weps.ee>
2017-05-18 12:35:46 +0300

    First

---
+++ music/models.py
@@ -0,0 +1,18 @@
+from __future__ import unicode_literals
+from django.db import models
+
+
+class Album(models.Model):
+    title = models.CharField(max_length=250)
+    genre = models.CharField(max_length=500)
+    artist = models.CharField(max_length=250)
+    cover = models.CharField(max_length=1000)
+
+    def __str__(self):
+        return self.title + ' - ' + self.artist + ', ' + self.genre
+
+
+class Song(models.Model):
+    album = models.ForeignKey(Album, on_delete=models.CASCADE)
+    fileType = models.CharField(max_length=10)
+    songTitle = models.CharField(max_length=250)
---
+++ music/views.py
@@ -0,0 +1,6 @@
+from __future__ import unicode_literals
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def index(request):
+    return HttpResponse('<h1>Hi</h1>')
